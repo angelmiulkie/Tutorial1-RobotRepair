f16c93a99d34fa6245932d793868a21452327bd8
Elaine Hoang <elahoang@uw.edu>
2026-04-10 00:37:44 -0700

    Finished Setup and Setup of a 2D Player Character

---
+++ Assets/_2DAdventureGame/Scripts/Player Controller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.y = position.y + 0.1f;
+        transform.position = position;
+    }
+}
