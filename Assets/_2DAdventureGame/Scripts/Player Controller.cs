4cd80b7f72766f67836c710e3d9d4b4036c8c0a4
Elaine Hoang <elahoang@uw.edu>
2026-04-14 23:34:42 -0700

    Finished Creating a Health Collectible Using Triggers

--- Assets/_2DAdventureGame/Scripts/Player Controller.cs
+++ Assets/_2DAdventureGame/Scripts/Player Controller.cs
@@ -5,34 +5,38 @@ public class PlayerController : MonoBehaviour
 {
 
     // Input Actions
+    // Character Movement
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
-    public int maxHealth = 5;
-    int currentHealth;
+    public float speed = 3.0f;
     Vector2 move;
+
+    // Health System
+    public int maxHealth = 5;
+    int currentHealth = 4;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
+        // currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+        // Debug.Log(move);
         
     }
 
     void FixedUpdate() {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount) {
+    public void ChangeHealth(int amount) {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
